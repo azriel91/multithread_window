05ac839e9285bff7590308e3cab09b2a3f92d286
Azriel Hoh <azriel91@gmail.com>
2018-07-01 14:58:38 +1200

    Consistently get `Xlib: sequence lost` errors when run as a loop.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
-extern crate winit;
+extern crate glutin;
 
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-use winit::{ControlFlow, EventsLoop, Window};
+use glutin::{GlContext, GlProfile, GlRequest};
 
 const THREAD_COUNT: usize = 10;
 
 fn open_window() {
-    let mut events_loop = EventsLoop::new();
-    let _window = Window::new(&events_loop).unwrap();
-    events_loop.run_forever(|_| ControlFlow::Break);
+    let mut events_loop = glutin::EventsLoop::new();
+
+    let context = glutin::ContextBuilder::new()
+        .with_multisampling(0)
+        .with_vsync(false)
+        .with_gl_profile(GlProfile::Core)
+        .with_gl(GlRequest::Latest);
+
+    let window = glutin::WindowBuilder::new();
+    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+
+    let _ = unsafe { gl_window.make_current() };
+
+    // println!(
+    //     "Pixel format of the window's GL context: {:?}",
+    //     gl_window.get_pixel_format()
+    // );
+
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| {
+            // println!("{:?}", event);
+            if let glutin::Event::WindowEvent { event, .. } = event {
+                match event {
+                    glutin::WindowEvent::CloseRequested => running = false,
+                    glutin::WindowEvent::Resized(logical_size) => {
+                        let dpi_factor = gl_window.get_hidpi_factor();
+                        gl_window.resize(logical_size.to_physical(dpi_factor));
+                    }
+                    _ => (),
+                }
+            }
+        });
+
+        let _ = gl_window.swap_buffers();
+    }
 }
 
 fn wait_for_threads<T>(thread_handles: Vec<JoinHandle<T>>) {
-    for handle in thread_handles.into_iter() {
+    for handle in thread_handles {
         let thread_id = handle.thread().id();
         match handle.join() {
             Ok(_) => println!("{:?}: success", thread_id),
@@ -27,13 +60,13 @@ fn main() {
     let mut thread_handles = vec![];
 
     for _ in 0..THREAD_COUNT {
-        let child = thread::spawn(|| open_window());
+        let child = thread::spawn(open_window);
         thread_handles.push(child);
     }
 
     thread::spawn(move || wait_for_threads(thread_handles));
     println!("Spawned threads.");
 
-    thread::sleep(Duration::from_millis(100));
-    println!("Waited for 100 ms.");
+    thread::sleep(Duration::from_millis(400));
+    println!("Waited for 400 ms.");
 }
